064907dfd1f9da72d9f1cc993f9b9f00b0801d46
Rujira Kongsomran <rujirabiwberry@gmail.com>
2023-05-23 21:36:32 +0700

    Activity Result API

---
+++ app/src/main/java/com/rujirakongsomran/kt_activityresultapi/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.rujirakongsomran.kt_activityresultapi
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import com.rujirakongsomran.kt_activityresultapi.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val getContent =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+
+
+        }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.btnSelect.setOnClickListener {
+            getContent.launch("image/*")
+        }
+//        setContentView(R.layout.activity_main)
+    }
+}
