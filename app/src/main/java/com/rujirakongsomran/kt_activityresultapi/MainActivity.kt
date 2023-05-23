8127cf272bdeb30966af235dbb92ce3281037dbb
Rujira Kongsomran <rujirabiwberry@gmail.com>
2023-05-23 21:56:57 +0700

    Call Visual Media Picker

--- app/src/main/java/com/rujirakongsomran/kt_activityresultapi/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_activityresultapi/MainActivity.kt
@@ -3,14 +3,21 @@ package com.rujirakongsomran.kt_activityresultapi
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import com.rujirakongsomran.kt_activityresultapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val getContent =
-        registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
 
+    //    private val getContent =
+//        registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+//
+//
+//        }
+    private val launcher: ActivityResultLauncher<PickVisualMediaRequest> =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri: Uri? ->
 
         }
 
@@ -21,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         binding.btnSelect.setOnClickListener {
-            getContent.launch("image/*")
+//            getContent.launch("image/*")
         }
 //        setContentView(R.layout.activity_main)
     }
